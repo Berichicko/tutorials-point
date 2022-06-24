8665f4b7290ee8e2ba0faa68666f54792041a46f
Aliaksandr Zasinets <aliaksandr.zasinets@gmail.com>
2022-06-24 17:35:02 +0300

    feat:tutorials-point

--- src/test/java/SeleniumDemoTest.java
+++ src/test/java/SeleniumDemoTest.java
@@ -4,28 +4,27 @@ import org.openqa.selenium.By;
 
 import static junit.framework.TestCase.assertEquals;
 
-public class SeleniumDemo extends Utils {
+public class SeleniumDemoTest extends WebDriverManager {
 
 
     @Test
-    public void initDriver() {
+    public void givenWindowMaxWhenGetThenInitialize() {
         driver.manage().window().maximize();
         driver.get("https://www.tutorialspoint.com/");
     }
 
     @Test
-    public void verifyTitle() {
+    public void givenTitleWhenAddThenReturn() {
         System.out.println("In Verify Title Test Case");
         String title = driver.getTitle();
         Assert.assertTrue(title.contains("Online Tutorials Library"));
     }
 
     @Test
-    public void verifyHeading() {
+    public void givenHeaderWhenAddThenReturn() {
         System.out.println("In Verify Heading Text Above Search Test Case");
         String expectedearchHeading = "  Enjoy theory and lab at the same time, right here online  ";
         String actualHeading = driver.findElement(By.xpath("html/body/div[3]/div[1]/div/div/div/h2")).getText();
         assertEquals(expectedearchHeading, actualHeading);
     }
 }
-
--- src/test/java/WebDriverManager.java
+++ src/test/java/WebDriverManager.java
@@ -1,16 +1,15 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Utils {
+public class WebDriverManager {
 
     public WebDriver driver;
 
     @Before
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
+        io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
 
